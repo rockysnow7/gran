21a8020ce8f5565c355572bd44554585fac87169
Finn Eyles <feyles@icloud.com>
2025-07-26 18:21:12 +0100

    rearranged files

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,8 @@
 pub mod sounds;
 pub mod player;
 pub mod effects;
-pub mod oscillators;
+pub mod oscillator;
+pub mod sample;
 
 pub use player::play_sound;
-pub use oscillators::Number;
+pub use oscillator::Number;
--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@
 
 use gran::{
     effects::{LowPassFilter, Saturation, TapeDelay, Volume},
-    oscillators::{note, ADSR, OscillatorBuilder, OscillatorInput, OscillatorInputAtTime, WaveFunction},
+    oscillator::{note, ADSR, OscillatorBuilder, OscillatorInput, OscillatorInputAtTime, WaveFunction},
     play_sound, Number,
     sounds::CompositionBuilder,
 };
@@ -15,18 +15,19 @@ async fn main() {
             amplitude: Number::number(0.7),
             phase: Number::number(0.0),
         })
-        .adsr(ADSR::new(1.0, 0.2, 0.5, 1.0))
+        .adsr(ADSR::new(0.4, 0.2, 0.5, 1.0))
         .effect(Box::new(Volume(Number::number(0.7))))
         .effect(Box::new(LowPassFilter::new(
             Number::sine_around(600.0, 200.0, 1.0),
-            Number::number(0.75),
+            // Number::number(0.75),
+            Number::square_around(0.3, 0.3, 2.0),
             4,
         )))
-        // .effect(Box::new(Saturation::new(
-        //     Number::number(3.0),
-        //     Number::number(0.5),
-        //     0.5,
-        // )))
+        .effect(Box::new(Saturation::new(
+            Number::number(3.0),
+            Number::number(0.5),
+            0.5,
+        )))
         .inputs(vec![
             OscillatorInputAtTime {
                 input: OscillatorInput::Press(note("C2")),
@@ -56,14 +57,14 @@ async fn main() {
         .build();
 
     let pink_noise = OscillatorBuilder::new()
-        .wave_function(WaveFunction::pink_noise(Number::number(0.001), 10))
+        .wave_function(WaveFunction::pink_noise(Number::number(0.0005), 10))
         .auto_play()
         .build();
 
     let mut composition = CompositionBuilder::new()
         .sound(Box::new(pink_noise))
         .sound(Box::new(bass))
-        .effect(Box::new(TapeDelay::light(0.5)))
+        .effect(Box::new(TapeDelay::light(0.2)))
         .build();
 
     play_sound(&mut composition);
